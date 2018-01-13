a89fa32484ae2c29b29b49511b662e6bf025019e
davidwells <david@inboundnow.com>
2018-01-13 13:48:34 -0800

    add loading state to talks

--- src/layouts/Talk/index.js
+++ src/layouts/Talk/index.js
@@ -3,6 +3,7 @@ import Post from '../Post'
 // import styles from './Talk.css'
 
 export default class Talk extends Component {
+  static hasLoadingState = true
   render() {
     return (
       <Post {...this.props} />
