ccf4541eb7e53d5e8ec3cd270dd2a44b8d612278
DavidWells <hello@davidwells.io>
2018-09-29 13:24:28 -0700

    disable auth0 lock

--- src/utils/auth/index.js
+++ src/utils/auth/index.js
@@ -15,33 +15,33 @@ let Auth0Lock
 let lockInstance // eslint-disable-line
 
 if (typeof window !== 'undefined') {
-  Auth0Lock = require('auth0-lock').default // eslint-disable-line
-  // init token
-  initializeXsrfToken()
-  const redirect = encodeURIComponent(window.location.href)
-  const redirectURL = `${window.location.origin}/loading/`
-  const state = `token=${getXsrfToken()}&url=${redirect}&other=lol`
-  // console.log('state', state)
-  // Configure Auth0
-  lockInstance = new Auth0Lock( // eslint-disable-line
-    process.env.AUTH0_CLIENT_ID,
-    process.env.AUTH0_DOMAIN, {
-      auth: {
-        redirectUrl: redirectURL,
-        responseType: 'token',
-        params: {
-          state,
-          // scope: 'openid email_verified',
-        },
-      },
-      theme: {
-        // logo: LogoImg,
-        primaryColor: '#000'
-      },
-      languageDictionary: {
-        title: 'Serverless'
-      }
-    })
+  // Auth0Lock = require('auth0-lock').default // eslint-disable-line
+  // // init token
+  // initializeXsrfToken()
+  // const redirect = encodeURIComponent(window.location.href)
+  // const redirectURL = `${window.location.origin}/loading/`
+  // const state = `token=${getXsrfToken()}&url=${redirect}&other=lol`
+  // // console.log('state', state)
+  // // Configure Auth0
+  // lockInstance = new Auth0Lock( // eslint-disable-line
+  //   process.env.AUTH0_CLIENT_ID,
+  //   process.env.AUTH0_DOMAIN, {
+  //     auth: {
+  //       redirectUrl: redirectURL,
+  //       responseType: 'token',
+  //       params: {
+  //         state,
+  //         // scope: 'openid email_verified',
+  //       },
+  //     },
+  //     theme: {
+  //       // logo: LogoImg,
+  //       primaryColor: '#000'
+  //     },
+  //     languageDictionary: {
+  //       title: 'Serverless'
+  //     }
+  //   })
 }
 
 export default lockInstance
