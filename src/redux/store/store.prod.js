734b5cde37cf3e962e9a155814b0741e4cb99272
DavidWells <hello@davidwells.io>
2018-09-29 13:21:11 -0700

    disable auth0

--- src/redux/store/store.prod.js
+++ src/redux/store/store.prod.js
@@ -2,16 +2,16 @@ import { combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import createStore from 'phenomic/lib/redux/createStore'
 import * as phenomicReducers from 'phenomic/lib/redux/modules'
-import authMiddleware from '../user-middleware'
-import userReducer from '../user'
+// import authMiddleware from '../user-middleware'
+// import userReducer from '../user'
 
-const extraMiddlewares = [thunk, authMiddleware]
+const extraMiddlewares = [thunk]
 
 export default function configureStore(preloadedState) {
   return createStore(
     combineReducers({
       ...phenomicReducers,
-      auth: userReducer,
+      // auth: userReducer,
     }),
     preloadedState,
     extraMiddlewares,
